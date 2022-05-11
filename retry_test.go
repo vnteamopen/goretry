ed5b618ac7a1319f78f2eff8fe1e4fe7190a5587
Thinh Nguyen <ngtrphuocthinh@gmail.com>
2022-05-11 22:52:06 +0700

    Add unit test for NoBackoff

--- retry_test.go
+++ retry_test.go
@@ -31,3 +31,35 @@ func TestDo(t *testing.T) {
 		t.Errorf("Do() expected duration: %d, actual: %d", expectedDuration, duration)
 	}
 }
+
+func TestNoBackoff(t *testing.T) {
+	var counting int64
+
+	start := time.Now()
+	to := time.Duration(0)
+	goretry.NoBackoff(func() error {
+		s1 := time.Now()
+		defer func() {
+			to += time.Since(s1)
+		}()
+		time.Sleep(100 * time.Millisecond)
+		counting++
+		if counting > 5 {
+			return nil
+		}
+		return errors.New("fake error")
+	})
+
+	duration := time.Since(start)
+
+	expectedCounting := int64(6)
+	if counting != expectedCounting {
+		t.Errorf("NoBackoff() expected counting: %d, actual: %d", expectedCounting, counting)
+	}
+
+	difference := duration - to
+	expectedDifference := time.Microsecond * 20
+	if difference > expectedDifference {
+		t.Errorf("NoBackoff() expected delay: %d, actual: %d", difference, expectedDifference)
+	}
+}
